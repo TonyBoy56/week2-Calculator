466e5ec6de94e98bffe6396890cc66d1d40630ae
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-23 11:47:00 -0700

    result returns a converted value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(number1) {
+  return (number1 * 9/5) + 32;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var Fahr = (number1 * 9/5) + 32;
+var result = number1 + " is " + Fahr + "F";
+
 alert(result);
+
+
